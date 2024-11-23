45057e8780fc9100b08ade3ea6b98c9094c91c87
ClebersonCosta <cleberson-costa@outlook.com>
2024-11-23 18:21:49 -0300

    :sparkles: feat: criada classe UrlData e adicionada novas funções na classe Main

---
+++ src/main/java/com/rocketseat/UrlData.java
@@ -0,0 +1,27 @@
+package com.rocketseat;
+
+public class UrlData {
+    private String originalUrl;
+    private Long expirationTime;
+
+    public UrlData(String originalUrl, Long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public Long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(Long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
